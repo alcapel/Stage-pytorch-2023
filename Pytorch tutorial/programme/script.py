531a4508faba91a3d53162cbc0c0de658032f24b
Eldohrim <alexcaapel@gamil.com>
2023-06-08 17:17:31 +0200

    update script.py'

--- Pytorch tutorial/programme/script.py
+++ Pytorch tutorial/programme/script.py
@@ -4,7 +4,7 @@ from models import Net
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
-from torch import nn
+from torch import nn    
 import torchvision.transforms as transforms 
 
 # Mise en place du réseau, des hyperparamètres et
@@ -17,7 +17,7 @@ epochs = 10
 # Importation des données et chargement
 trainset = CIFAR10(root='./data', train=True, download=True,transform=transforms.ToTensor())
 trainCIFAR10 = DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
-testset = CIFAR10(root='./data', train= False, download=True,transform=transforms.ToTensor())
+testset = CIFAR10(root='./data', train=False, download=True,transform=transforms.ToTensor())
 testCIFAR10 = DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
 
 # Apprentissage et tests
@@ -25,5 +25,5 @@ for i in range(epochs):
     print(f"Boucle n°{i+1} ------------------------------\n")
     train(trainCIFAR10, net, optimizer, loss)
     test(testCIFAR10, net)
-    
+
 print("C'est terminé !")
