ccbc3dfae440a7899219deb268a353169ca4242e
Eldohrim <alexcaapel@gamil.com>
2023-07-13 15:20:19 +0200

    Update 13/07

--- Code/crowd_models/crowd_learning.py
+++ Code/crowd_models/crowd_learning.py
@@ -5,7 +5,7 @@ from torch.nn.functional import softmax
 from peerannot.helpers.networks import networks
 import torch.optim as optim
 import torch.nn as nn
-from models import worker_pred
+from models import worker_pred, get_workers
 import gc
 
 # %%
@@ -19,7 +19,6 @@ n_classe = len(classe)
 t = worker_json(obj_train, get_nb=False)
 n_worker = len(t)
 
-
 #%% Création dataset avec les distribution
 n = len(obj_train)
 dataset = []
@@ -32,162 +31,29 @@ batch_size = 16
 trainset = DataLoader(dataset, batch_size=batch_size,
                       shuffle=True, num_workers=2)
 
-# %%
-# liste_mod = []
-# for i in range(2571):
-#     model = networks('resnet18', n_classes=10, pretrained=False).to("cuda")
-#     model.load_state_dict(torch.load(f'./expert_models/model-{i}_weights.pth'))
-#     liste_mod.append(model)
-
-# print("Est ce qu'il y a tous les modèles ?", len(liste_mod)==2571)
 #%%
 # On a au plus 63 personnes qui ont voté sur une image et 
 # une personne a voté au moins sur 181 images.
-w = torch.ones(n_worker, dtype=float, requires_grad=True)/n_worker
-param = nn.Parameter(w)
-
 
 #%%
-def get_img(img, dataset):
-    i = 0
-    while not(torch.equal(img, dataset[i][0])):
-        i=i+1
-    return i 
-
-#%%
-# Datajouet 
-liste = []
-
-for i in range(1000):
-    lab = torch.rand(10)
-    lab /= lab.sum()
-    x = torch.rand((10,37))
-    liste.append((x,lab))
-
-datatest = DataLoader(liste, batch_size=batch_size,
-                      shuffle=True, num_workers=2)
-#%%
-import torch.optim as optim
-optimizer = optim.SGD([param], lr=0.01,momentum=0.9)
-loss = nn.CrossEntropyLoss()
-
-for j in range(50):
-    for batch, (X, y) in enumerate(datatest):
-        p = torch.zeros((32,10))
-        if batch<31:
-            optimizer.zero_grad()
-            for i in range(batch_size):
-                p[i] = X[i].matmul(param.float())
-            lossRes = loss(p,y)
-            lossRes.backward()
-            optimizer.step()
-
-#%%
-k=0
-count = 0
-for i in range(2571):
-    for j in range(2571):
-        if (i<j):
-            a = list(set(t[f"{i}"].keys())&set(t[f"{j}"].keys()))
-            if len(a)>k:
-                if len(a)==len(t[f"{i}"].keys()):
-                    count+=1
-                else:   
-                    k=len(a)
-                    print(i, j, k)
-#%%
-
-k=0
-liste = []
-for i in range(2571):
-    for j in range(2571):
-        if (i<j)&(i not in liste):
-            a = list(set(t[f"{i}"].keys())&set(t[f"{j}"].keys()))
-            if len(a)==len(t[f"{i}"].keys()):
-                count+=1
-                liste.append(j)
-
-# %%
-import torch.optim as optim
-w = torch.ones(n_worker, dtype=float, requires_grad=True)/n_worker
-param = nn.Parameter(w, requires_grad=True)
-optimizer = optim.SGD([param], lr=0.01,momentum=0.9)
-loss = nn.CrossEntropyLoss()
-model = networks('resnet18', n_classes=10, pretrained=False).to("cuda")
-
-for batch, (X, lab) in enumerate(trainset):
-    optimizer.zero_grad()
-    p = torch.zeros((batch_size, n_classe), requires_grad=False).to("cuda")
-    for i in range(batch_size):
-        task = get_img(X[i],dataset)   # on cherche à avoir accès à l'image pour pouvoir savoir qui a voté
-        worker = list(obj_train[f"{task}"].keys())   # liste de qui vote
-        tot = 0
-        for k in worker:
-            p[i] = p[i] + worker_pred(model, X, int(k))[i]*param[int(k)]    #prédiction du worker k sur l'item i
-            tot = tot + param[int(k)]
-        p[i] = p[i]/tot
-    lossRes = loss(p, lab.to("cuda"))
-    lossRes.backward()
-    optimizer.step()
-    # l'article ne précise pas si il faut softmax les poids 
-    # pendant la boucle ou à la fin.
-
-# %%
-import torch.optim as optim
-w = torch.ones(n_worker, dtype=float, requires_grad=True)/n_worker
-param = nn.Parameter(w, requires_grad=True)
-optimizer = optim.SGD([param], lr=0.01,momentum=0.9)
-loss = nn.CrossEntropyLoss()
-
-for batch, (X, lab) in enumerate(trainset):
-    optimizer.zero_grad()
-    pred = torch.zeros((batch_size, n_classe), requires_grad=False).to("cuda")
-    for i in range(batch_size):
-        model = networks('resnet18', n_classes=10, pretrained=False).to("cuda")
-
-        task = get_img(X[i],dataset)   # on cherche à avoir accès à l'image pour pouvoir savoir qui a voté
-        worker = list(obj_train[f"{task}"].keys())   # liste de qui vote
-        tot = 0
-        p = torch.zeros((n_worker, n_classe), requires_grad=False)
-        sel_w = torch.zeros((n_worker,1))
-        for k in worker:
-            p[int(k)] = worker_pred(model, X, int(k))[i] #prédiction du worker k sur l'item i
-            sel_w[int(k)] = 1
-        w = param.clone()
-        pred[i] = p.t().matmul(w.float())
-        del model
-        gc.collect()
-        torch.cuda.empty_cache()
-    with torch.autograd.set_detect_anomaly(True):
-        lossRes = loss(pred, lab.to("cuda"))
-    lossRes.backward()
-    optimizer.step()
-    
-    # l'article ne précise pas si il faut softmax les poids 
-    # pendant la boucle ou à la fin.
-#%%
-import torch.optim as optim
-
+tot_worker = list(t.keys())
 w = torch.ones(n_worker, dtype=torch.float, requires_grad=True) / n_worker
 param = nn.Parameter(w)
 optimizer = optim.SGD([param], lr=0.01, momentum=0.9)
 loss = nn.CrossEntropyLoss()
-torch.autograd.set_detect_anomaly(True)
 
 for batch, (X, lab) in enumerate(trainset):
     optimizer.zero_grad()
     pred = torch.zeros((batch_size, n_classe)).to("cuda")
     model = networks('resnet18', n_classes=10, pretrained=False).to("cuda")
     for i in range(batch_size):
-        task = get_img(X[i], dataset)
-        worker = list(obj_train[f"{task}"].keys())
-        tot = 0
+        work_t = get_workers(X[i], dataset, obj_train, tot_worker)
         p = torch.zeros((n_worker, n_classe))
         sel_w = torch.zeros((n_worker, 1))
-        for k in worker:
+        for k in work_t:
             p[int(k)] = worker_pred(model, X, int(k))[i].detach()
             sel_w[int(k)] = 1
-        pred[i] = p.t().matmul(param)
+        pred[i] = (1/sel_w.t().matmul(param))*p.t().matmul(param)
     del model
     gc.collect()
     torch.cuda.empty_cache()
@@ -195,4 +61,5 @@ for batch, (X, lab) in enumerate(trainset):
     lossRes.backward()
     optimizer.step()
 
-# %%
+# l'article ne précise pas si il faut softmax les poids 
+# pendant la boucle ou à la fin.
--- Code/crowd_models/models.py
+++ Code/crowd_models/models.py
@@ -18,3 +18,21 @@ def worker_pred(model, data, worker):
     model.load_state_dict(torch.load(f'./expert_models/model-{worker}_weights.pth'))
     model.eval()
     return  softmax(model(data.to("cuda").float()), dim=1)
+
+def get_img(img, dataset):
+    i = 0
+    while not(torch.equal(img, dataset[i][0])):
+        i=i+1
+    return i 
+
+def diff(liste1, liste2):
+    res = []
+    for i in liste1:
+        if i not in liste2:
+            res.append(i)
+    return res
+
+def get_workers(img, dataset, json, tot_worker):
+    task = get_img(img, dataset)
+    worker = list(json[f"{task}"].keys())
+    return diff(tot_worker, worker)
